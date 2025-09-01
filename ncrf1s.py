07e86974fc7550e5414320533682bfa1646196de
Maryam Valian <maryam.valian@gmail.com>
2025-09-01 15:52:45 -0400

    one subject fit

--- ncrf1s.py
+++ ncrf1s.py
@@ -19,20 +19,23 @@ from mne import *
 from ncrf import fit_ncrf
 from pathlib import Path
 import numpy as np
+from eelbrain import NDVar, UTS
+from eelbrain import plot, combine
 
 # %%
 subject="sub-01"
 root = Path("~/Data/ds005810")
 #clean_fif = root / f"derivatives/preprocessed/raw/{subject}_ses-ImageNet01_task-ImageNet_run-01_meg_clean.fif"
 raw_fif=root/f"{subject}/ses-ImageNet01/meg/{subject}_ses-ImageNet01_task-ImageNet_run-01_meg.fif"
+empty_room=root/"sub-emptyroom/ses-20211114/meg/sub-emptyroom_ses-20211114_task-noise_meg.fif"
 
 # %%
-raw = mne.io.read_raw_fif(raw_fif, preload=False)
+raw = mne.io.read_raw_fif(raw_fif, preload=True)
 
 events = find_events(raw, stim_channel="UPPT001")
 #raw.info["bads"]  
-#raw
-raw.n_times
+raw
+#raw.n_times
 
 # %%
 stim = events[events[:, 2] == 2]   #ID2 : stim_on
@@ -63,3 +66,56 @@ event_table = pd.DataFrame({
 event_table
 
 # %%
+meg.first_time   
+
+# %%
+meg=raw.pick("meg")
+meg.filter(1., 40., phase="zero-double", verbose=False)
+meg.resample(100, npad="auto", verbose=False)
+
+sfreq = meg.info['sfreq']         
+n_times = meg.n_times              
+     
+
+
+time = UTS(0, 1/sfreq, n_times)
+
+
+stim1 = np.zeros(n_times, dtype=float)  #inanimate
+stim2 = np.zeros(n_times, dtype=float)  #animate
+
+
+for t, is_anim in zip(event_table['time'], event_table['animate']):
+    idx = int(round((t ) * sfreq))
+    if 0 <= idx < n_times:
+        if is_anim:
+            stim2[idx] = 1.0
+        else:
+            stim1[idx] = 1.0
+
+
+stim1 = NDVar(stim1, time)
+stim2 = NDVar(stim2, time)
+
+
+p=plot.LineStack(combine([stim1, stim2]), ylabels=["inanimate", "animate"], offset=1.5)
+
+# %%
+stim1.x
+
+# %%
+stim1.x[738]
+
+# %% [markdown]
+# ## NOISE COv
+
+# %%
+raw_er = mne.io.read_raw_fif(empty_room, preload=True, verbose=False).pick('meg')
+raw_er.filter(1., 40., phase="zero-double", verbose=False)
+raw_er.resample(100, npad="auto", verbose=False)
+
+noise_cov = mne.compute_raw_covariance(raw_er, method='shrunk', rank=None,verbose=False)
+noise_cov
+
+# %%
+raw_er.filter(1., 40., phase="zero-double", verbose=False)
