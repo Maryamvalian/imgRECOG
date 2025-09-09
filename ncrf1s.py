a07bfdc41a1122cfeca0a41277a566bef1f670c4
Maryam Valian <maryam.valian@gmail.com>
2025-09-09 12:41:57 -0400

    mne inverse fixed morphing

--- ncrf1s.py
+++ ncrf1s.py
@@ -26,23 +26,20 @@ from eelbrain import *
 # %%
 subject="sub-02"
 run="01"
-session="ImageNet02"                          
+session="ImageNet02" 
+
 root = Path("~/Data/ds005810")
 subjects_dir = str(Path('~/Data/ds005810/derivatives/freesurfer/subjects').expanduser())
-
 
 clean_fif = root / f"derivatives/preprocessed/raw/{subject}_ses-{session}_task-ImageNet_run-{run}_meg_clean.fif"
 raw_fif=root/f"{subject}/ses-{session}/meg/{subject}_ses-{session}_task-ImageNet_run-{run}_meg.fif"
 empty_room=root/"sub-emptyroom/ses-20211114/meg/sub-emptyroom_ses-20211114_task-noise_meg.fif"
-
-src_file = f"{subjects_dir}/{subject}/bem/{subject}-vol-7-src.fif"
 
 # %%
 raw = mne.io.read_raw_fif(raw_fif, preload=True,verbose=False)
 clean= mne.io.read_raw_fif(clean_fif, preload=True,verbose=False)
 events = find_events(raw, stim_channel="UPPT001")
 #raw.info["bads"]  
-clean
 #raw.n_times
 fig = mne.viz.plot_events(events, sfreq=raw.info["sfreq"], first_samp=raw.first_samp)
 
@@ -132,17 +129,12 @@ noise_cov
 # # Lead FIeld
 
 # %%
-
+src_file = f"{subjects_dir}/{subject}/bem/{subject}-vol-7-src.fif"
 src = mne.read_source_spaces(str(src_file),verbose=False)
-src
 
-# %%
-mindist=0 #same src size for all subjects
-#bem_model = mne.make_bem_model(subject, ico=4, conductivity=(0.3,),verbose=False)
-#bem_sol   = mne.make_bem_solution(bem_model,verbose=False)
 bem_sol_fif=f"{subjects_dir}/{subject}/bem/{subject}-bem-sol.fif"
-bem_sol = mne.read_bem_solution(bem_sol_fif)
-print(f"BEM SOLUTION: {bem_sol}")
+bem_sol = mne.read_bem_solution(bem_sol_fif,verbose=False)
+
 
 trans_fif= f"{root}/derivatives/trans/{subject}-{session}-trans.fif"
 trans=mne.read_trans(trans_fif)
@@ -150,18 +142,19 @@ trans=mne.read_trans(trans_fif)
 fwd = mne.make_forward_solution(meg.info,trans ,
                                 src, bem_sol,
                                 meg=True, eeg=False,
-                                mindist=mindist,
+                                mindist=0,               #======same size with src
                                 verbose=False
                                )
-
-#Print(f"FWD src={len(fwd['src'][0]['vertno'])}")
 
 #convert fwd to ndvar
 lf = load.mne.forward_operator(fwd,src='vol-7',
                                subjects_dir=subjects_dir,
                                adjacency=False,parc='aparc+aseg') 
 lf  = lf.sub(sensor=meg_ndvar.sensor)
-lf
+
+# %%
+print(len(src[0]['vertno']))
+print(len(fwd['src'][0]['vertno']))  
 
 # %%
 print(f"Fitting NCRF")
@@ -198,7 +191,7 @@ h = hlist[0]
 # %%
 h= morph_source_space(
     h,
-    subject_to='fsaverage',
+    subject_to='fsaverage2',
     copy=True,
     
     )
@@ -217,14 +210,20 @@ for t in times:
 # ## MNE inverse
 
 # %%
-clean.filter(1., 40., phase="zero-double", verbose=False)
-clean.resample(100, npad="auto", verbose=False)
-clean_ndvar = load.fiff.raw_ndvar(clean)
-clean_ndvar
+raw = mne.io.read_raw_fif(raw_fif, preload=True,verbose=False)
+events = find_events(raw, stim_channel="UPPT001") #origin
+
+raw2=raw
+raw2.filter(1., 40., phase="zero-double", verbose=False)
+
+
+rs_raw , rs_events = raw2.resample(100, npad="auto", events=events, stim_picks="UPPT001", verbose=False)
+
+rs_raw_ndvar = load.fiff.raw_ndvar(rs_raw)
 
 # %%
 epochs =mne.Epochs(
-    clean, events,          #_____
+    rs_raw, rs_events,          #_____
     tmin=-0.1,tmax=0.8,
     
     event_id={'stim_on': 2},
@@ -234,7 +233,7 @@ epochs =mne.Epochs(
 evoked = epochs.average()
 
 inv= mne.minimum_norm.make_inverse_operator(
-    info=clean.info, forward=fwd, noise_cov=noise_cov,   #_____
+    info=rs_raw.info, forward=fwd, noise_cov=noise_cov,   #_____
     loose=1.0,    
     depth=0.8,
     fixed=False,
@@ -252,144 +251,38 @@ stc_vec = mne.minimum_norm.apply_inverse(
     
 )
 
-stc_nd = load.mne.stc_ndvar(
-    stc_vec,
-    src='vol-7',                         
+# %%
+src_fs2 = mne.read_source_spaces(f"{subjects_dir}/fsaverage2/bem/fsaverage2-vol-7-src.fif",verbose=False)
+len(src_fs2[0]['vertno'])
+
+# %%
+morph = mne.compute_source_morph(
+    src=src,
+    subject_from=subject,
+    subject_to="fsaverage2",     
     subjects_dir=subjects_dir,
-    subject=subject,
+    spacing=7.0, 
+    src_to=src_fs2,                                     #========force to use destination src size
+    precompute=True,
+    verbose=True,
 )
-
-stc_nd
-
-# %%
-morphed_stc = morph_source_space(                   
-    stc_nd,
-    subject_to='fsaverage',
-    copy=True,
-    
-    )
+stc_vec_fs = morph.apply(stc_vec)
 
 # %%
-p = plot.Butterfly(stc_nd.norm('space'), color='k')
+stc_nd_fs = load.mne.stc_ndvar(
+    stc_vec_fs,
+    src='vol-7',                  # matches fsaverage2-vol-7-src.fif 
+    subjects_dir=subjects_dir,
+    subject='fsaverage2',
+)
+stc_nd_fs
+
+# %%
+p = plot.Butterfly(stc_nd_fs.norm('space'), color='k')
 times = [0.12,0.17,0.24,0.37,0.48,0.55]
 for t in times:
     p.add_vline(t)
 for t in times:
-    f = plot.GlassBrain(stc_nd.sub(time=t),title=f"stim_on morphed, {t}s") 
-
-# %%
-clean=clean.pick("meg")
-clean.filter(1., 40., phase="zero-double", verbose=False)
-clean.resample(100, npad="auto", verbose=False)
-
-sfreq = clean.info['sfreq']     #100       
-n_times = clean.n_times         #32000     
-
-clean_ndvar = load.fiff.raw_ndvar(clean)
-print(f"MEG: {clean_ndvar}")
-     
-
-
-time = UTS(0, 1/sfreq, n_times)      #from 0 to 32000/100=320 s
-
-
-stim1 = np.zeros(n_times, dtype=float)  
-stim2 = np.zeros(n_times, dtype=float) 
-
-
-for t, is_anim in zip(event_table['time'], event_table['animate']):
-    idx = int(round((t ) * sfreq))
-    if 0 <= idx < n_times:
-        if is_anim:
-            stim2[idx] = 1.0
-        else:
-            stim1[idx] = 1.0
-
-
-stim1 = NDVar(stim1, time)
-stim2 = NDVar(stim2, time)
-
-# %%
-mindist=0 #same src size for all subjects
-bem_model = mne.make_bem_model(subject, ico=4, conductivity=(0.3,),verbose=False)
-bem_sol   = mne.make_bem_solution(bem_model,verbose=False)
-
-trans_fif= f"{root}/derivatives/trans/{subject}-{session}-trans.fif"
-trans=mne.read_trans(trans_fif)
-
-fwd = mne.make_forward_solution(clean.info,trans ,
-                                src, bem_sol,
-                                meg=True, eeg=False,
-                                mindist=mindist,
-                                verbose=False
-                               )
-print(fwd)
-
-
-
-#convert mne fwd to ndvar
-lf = load.mne.forward_operator(fwd,src='vol-7',
-                               subjects_dir=subjects_dir,
-                               adjacency=False,parc='aparc+aseg') 
-lf  = lf.sub(sensor=clean_ndvar.sensor)
-lf
-
-# %%
-
-args = (
-            clean_ndvar,
-            [stim1,stim2],
-            lf,
-            noise_cov,
-            0,
-            0.8,
-        )
-kwargs = {'normalize': 'l1',
-                  'in_place': False,
-                  'mu':'auto',
-                  'verbose': True, 
-                  'n_iter': 10,
-                  'n_iterc': 10,
-                  'n_iterf': 100} 
-model = fit_ncrf(*args, **kwargs)  
-
-#---------------
-save_dir = Path('base_ncrf')
-save_dir.mkdir(exist_ok=True)
-
-filename = save_dir / f"{subject}.pickle"
-save.pickle(model, filename)
-
-hlist=model.h
-hlist
-
-# %%
-h = hlist[0]
-                      
-
-h= morph_source_space(
-    h,
-    subject_to='fsaverage',
-    copy=True,
-    
-    )
-
-
-
-h = h.smooth('source', 0.01, 'gaussian')
-p = plot.Butterfly(h.norm('space'), color='k')
-times = [0.12,0.17,0.24,0.37,0.48,0.55,0.66,0.77]
-for t in times:
-    p.add_vline(t)
-for t in times:
-    f = plot.GlassBrain(h.sub(time=t),title=f"ALL words, {t}s")  
-
-# %%
-
-# %%
-
-# %%
-
-# %%
+    f = plot.GlassBrain(stc_nd_fs.sub(time=t),title=f"stim_on morphed, {t}s") 
 
 # %%
