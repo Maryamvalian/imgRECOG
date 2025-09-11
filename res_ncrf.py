94c407127676b7ceabe36d7846f9b38c81609fa1
Maryam Valian <maryam.valian@gmail.com>
2025-09-11 16:27:18 -0400

    up

--- res_ncrf.py
+++ res_ncrf.py
@@ -22,7 +22,7 @@ subjects_dir = str(Path('~/Data/ds005810/derivatives/freesurfer/subjects').expan
 
 # %%
 cases = []
-for i in range(1, 8):
+for i in range(1, 9):
     subject = f"sub-{i:02d}"
     morphed_file = f"base_ncrf/{subject}-morphed.pickle"
     
@@ -64,3 +64,73 @@ res = testnd.VectorDifferenceRelated(
     tstop=0.6,
     samples=1000
 )
+
+# %%
+diff= res.masked_difference()
+p = plot.Butterfly(diff.norm('space'), color='k',title='anim VS inanim')
+times = [0.21]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(diff.sub(time=t),title=f"anim vs inan, {t}s")  
+
+# %% [markdown]
+# ## one sample test
+
+# %%
+data_inan = data.sub("type == 'inanimate'")
+result_inan = testnd.Vector('ncrf', match='subject', data=data_inan, tfce=True, tstart=0.1, tstop=0.6,samples=1000)
+
+# %%
+p = plot.Butterfly(result_inan.masked_difference().norm('space'), color='k')
+times = [0.12]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(result_inan.masked_difference().sub(time=t),title=f"Inanimate, {t}s")  
+
+# %% [markdown]
+# ## aggregate
+
+# %%
+agg=data.aggregate('type', drop_bad=True)
+agg_in = agg.sub("type == 'inanimate'")
+agg_an = agg.sub("type == 'animate'")
+diff=agg_an['ncrf']- agg_in['ncrf']
+p = plot.Butterfly(diff.norm('space'), color='k')
+times = [0.12,0.17,0.28,0.45]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(diff.sub(time=t),title=f"animate, {t}s") 
+
+# %%
+agg.head()
+
+
+# %%
+p = plot.Butterfly(anim.norm('space'), color='k')
+times = [0.12,0.17,0.28,0.45]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(anim.sub(time=t),title=f"one subject inanimate, {t}s") 
+
+# %%
+p = plot.Butterfly(inanim.norm('space'), color='k')
+times = [0.12,0.17,0.28,0.45]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(inanim.sub(time=t),title=f"one subject animate, {t}s") 
+
+# %%
+stc_diff=inanim-anim
+p = plot.Butterfly(stc_diff.norm('space'), color='k')
+times = [0.12,0.17,0.28,0.45,0.56,0.65,0.72]
+for t in times:
+    p.add_vline(t)
+for t in times:
+    f = plot.GlassBrain(stc_diff.sub(time=t),title=f"one subject animate, {t}s") 
+
+# %%
