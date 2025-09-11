204ae40916f20ba01b25fcc49f2e51682863b3f8
Maryam Valian <maryam.valian@gmail.com>
2025-09-11 14:01:13 -0400

    first

---
+++ res_ncrf.py
@@ -0,0 +1,45 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+from eelbrain import *
+from pathlib import Path
+from morph_nd import morph_nd
+
+subjects_dir = str(Path('~/Data/ds005810/derivatives/freesurfer/subjects').expanduser())
+
+# %%
+cases = []
+for i in range(1, 7):
+    
+    subject = f"sub-{i:02d}"
+    model_file=f"base_ncrf/{subject}.pickle"
+    model = load.unpickle(model_file)
+    hlist=model.h
+    inanim=hlist[0]
+    anim=hlist[1]
+    
+    anim_fs = morph_nd(subject, 'fsaverage2', subjects_dir, anim, 'vol-7')
+    inanim_fs = morph_nd(subject, 'fsaverage2', subjects_dir, inanim, 'vol-7')
+    
+    anim = anim_fs.smooth('source', 0.01, 'gaussian')
+    inanim = inanim_fs.smooth('source', 0.01, 'gaussian')
+
+    cases.append([subject, 'inanimate', inanim])
+    cases.append([subject, 'animate', anim])
+    print(f"{subject} model extracted")
+
+data = Dataset.from_caselist(['subject', 'type', 'ncrf'], cases)    
+
+# %%
